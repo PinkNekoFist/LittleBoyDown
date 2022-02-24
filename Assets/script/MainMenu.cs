401ede37e18d6093623d421dc848a733a5fbf8be
Diana <noah940407@gmail.com>
2022-02-24 10:30:07 +0800

    .

---
+++ Assets/script/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void quitGame()
+    {
+        Debug.Log("QUIT!"); 
+        Application.Quit();
+    }
+    public void playAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
